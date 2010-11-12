cea614840abec0f793514af15dcce5bf68bcd45d
waldir <waldir@email.com>
2010-11-12 02:56:33 +0000

    update

--- chrome/content/ff-overlay.js
+++ chrome/content/ff-overlay.js
@@ -8,4 +8,35 @@ procrastinever.showFirefoxContextMenu = function(event) {
   document.getElementById("context-procrastinever").hidden = gContextMenu.onImage;
 };
 
+procrastinever.getUsername = function() {
+  var username = prompt("Please insert your Wakoopa username:");
+  if (!username) return;
+  x=new(window.ActiveXObject||XMLHttpRequest)('Microsoft.XMLHTTP');
+  x.open('GET',"http://api.wakoopa.com/"+username+"/software.json?limit=1",true);
+  x.onreadystatechange=function() {
+    if (x.readyState==4 && x.status==200){
+      if(x.responseText.indexOf("wakoopaApi") != 0) {
+        alert("Username '"+username+"' not found on the Wakoopa servers");
+      }
+      else {
+        var usernamePref = "extensions.procrastinever.wakoopa-username";
+        Application.prefs.setValue(usernamePref, username);
+      }
+    }
+  };
+  x.send();
+}
+
+procrastinever.onToolbarButtonCommand = function(event) {
+  var usernamePref = "extensions.procrastinever.wakoopa-username";
+  // If we don't have the user's Wakoopa username, ask for it
+  if(!Application.prefs.getValue(usernamePref, false)) {
+    procrastinever.getUsername();
+  }
+  // Otherwise, get the data
+  else{
+    alert("we're working on it :)");
+  }
+};
+
 window.addEventListener("load", procrastinever.onFirefoxLoad, false);
--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,6 +1,6 @@
 pref("extensions.procrastinever.boolpref", false);
 pref("extensions.procrastinever.intpref", 0);
-pref("extensions.procrastinever.stringpref", "A string");
+pref("extensions.procrastinever.wakoopa-username", "");
 
 // See http://kb.mozillazine.org/Localize_extension_descriptions
 pref("extensions.procrastinever@codebits.eu.description", "chrome://procrastinever/locale/procrastinever.properties");
