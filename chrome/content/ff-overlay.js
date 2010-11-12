1f6817bbd7a83a1b320405f6522e415fd9f10491
waldir <waldir@email.com>
2010-11-12 12:07:11 +0000

    fix short username bug, merge overlay.js into ff-overlay.js, and some other changes

--- chrome/content/ff-overlay.js
+++ chrome/content/ff-overlay.js
@@ -1,42 +1,48 @@
-procrastinever.onFirefoxLoad = function(event) {
-  document.getElementById("contentAreaContextMenu")
-          .addEventListener("popupshowing", function (e){ procrastinever.showFirefoxContextMenu(e); }, false);
-};
-
-procrastinever.showFirefoxContextMenu = function(event) {
-  // show or hide the menuitem based on what the context menu is on
-  document.getElementById("context-procrastinever").hidden = gContextMenu.onImage;
-};
-
-procrastinever.getUsername = function() {
-  var username = prompt("Please insert your Wakoopa username:");
-  if (!username) return;
-  x=new(window.ActiveXObject||XMLHttpRequest)('Microsoft.XMLHTTP');
-  x.open('GET',"http://api.wakoopa.com/"+username+"/software.json?limit=1",true);
-  x.onreadystatechange=function() {
-    if (x.readyState==4 && x.status==200){
-      if(x.responseText.indexOf("wakoopaApi") != 0) {
-        alert("Username '"+username+"' not found on the Wakoopa servers");
-      }
-      else {
-        var usernamePref = "extensions.procrastinever.wakoopa-username";
-        Application.prefs.setValue(usernamePref, username);
-      }
-    }
+var procrastinever = {
+/*
+  onFirefoxLoad: function(event) {
+    document.getElementById("contentAreaContextMenu")
+            .addEventListener("popupshowing", function (e){ procrastinever.showFirefoxContextMenu(e); }, false);
   };
-  x.send();
-}
 
-procrastinever.onToolbarButtonCommand = function(event) {
-  var usernamePref = "extensions.procrastinever.wakoopa-username";
-  // If we don't have the user's Wakoopa username, ask for it
-  if(!Application.prefs.getValue(usernamePref, false)) {
-    procrastinever.getUsername();
-  }
-  // Otherwise, get the data
-  else{
-    alert("we're working on it :)");
+  showFirefoxContextMenu: function(event) {
+    // show or hide the menuitem based on what the context menu is on
+    document.getElementById("context-procrastinever").hidden = gContextMenu.onImage;
+  };*/
+
+  getUsername: function() {
+    do{
+      var username = prompt("Please insert your Wakoopa username:");
+      if(username.length < 3) alert("The username must have 3 or more characters.");
+    } while(username.length < 3);
+
+    x=new(window.ActiveXObject||XMLHttpRequest)('Microsoft.XMLHTTP');
+    x.open('GET', "http://api.wakoopa.com/"+username+"/software.json?limit=1", true);
+    x.onreadystatechange=function() {
+      if (x.readyState==4){
+        if(x.status==200){
+          var usernamePref = "extensions.procrastinever.wakoopa-username";
+          Application.prefs.setValue(usernamePref, username);
+        }
+        else {
+          alert("Username '"+username+"' not found on the Wakoopa servers");
+        }
+      }
+    };
+    x.send();
+  },
+
+  onToolbarButtonCommand: function(event) {
+    var usernamePref = "extensions.procrastinever.wakoopa-username";
+    // If we don't have the user's Wakoopa username, ask for it
+    if(!Application.prefs.getValue(usernamePref, false)) {
+      procrastinever.getUsername();
+    }
+    // Otherwise, get the data
+    else{
+      alert("we're working on it :)");
+    }
   }
 };
 
-window.addEventListener("load", procrastinever.onFirefoxLoad, false);
+//window.addEventListener("load", procrastinever.onFirefoxLoad, false);
